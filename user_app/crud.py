9af9539254efd1a3cb2772b6ccf20273dcfcabde
pOsdas <ratmir1099@mail.ru>
2025-04-18 23:29:14 +0300

    fixed handling of 'Not Found' error in GetUserByUsernameAPIView

--- user_app/crud.py
+++ user_app/crud.py
@@ -36,7 +36,7 @@ def get_user_by_username(
         user = User.objects.get(username=username)
         return user
     except ObjectDoesNotExist:
-        return None
+        raise
 
 
 def get_user_by_email(
